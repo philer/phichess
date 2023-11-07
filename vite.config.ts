610bfa1bb9f6132aef9fc6387839c77279ac96bb
philer <me@philer.org>
2023-11-07 01:59:29 +0100

    clean up vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -2,21 +2,9 @@ import { svelte } from "@sveltejs/vite-plugin-svelte"
 import { defineConfig } from "vite"
 
 // https://vitejs.dev/config/
-export default defineConfig(({ mode }) => ({
+export default defineConfig(() => ({
   build: {
     target: "es2022",
   },
-  // css: {
-  //   modules: {
-  //     generateScopedName: mode === "development"
-  //       ? "[name]_[local]_[hash:base64:5]"
-  //       : "[hash:base64:6]",
-  //   },
-  //   preprocessorOptions: {
-  //     scss: {
-  //       charset: false,
-  //     },
-  //   },
-  // },
   plugins: [svelte()],
 }))
