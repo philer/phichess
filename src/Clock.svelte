9ef80dd885f54fac753bf0accee44eb7c3e81bfe
philer <me@philer.org>
2023-11-09 21:18:48 +0100

    add clock icons

--- src/Clock.svelte
+++ src/Clock.svelte
@@ -1,8 +1,24 @@
 <script context="module" lang="ts">
+  import {
+    mdiClockTimeEightOutline,
+    mdiClockTimeElevenOutline,
+    mdiClockTimeFiveOutline,
+    mdiClockTimeFourOutline,
+    mdiClockTimeNineOutline,
+    mdiClockTimeOneOutline,
+    mdiClockTimeSevenOutline,
+    mdiClockTimeSixOutline,
+    mdiClockTimeTenOutline,
+    mdiClockTimeThreeOutline,
+    mdiClockTimeTwelveOutline,
+    mdiClockTimeTwoOutline,
+    mdiPause,
+  } from "@mdi/js"
   import { getContext, setContext } from "svelte"
   import { type Readable, readonly, writable } from "svelte/store"
 
   import type { Color } from "./chess"
+  import Icon from "./Icon.svelte"
 
   export type ClockState = Readonly<{
     running: boolean
@@ -75,6 +91,20 @@
   }
 
   const doubleDigit = (x: number) => Math.floor(x).toString().padStart(2, "0")
+  const clockIcons = [
+    mdiClockTimeOneOutline,
+    mdiClockTimeTwoOutline,
+    mdiClockTimeThreeOutline,
+    mdiClockTimeFourOutline,
+    mdiClockTimeFiveOutline,
+    mdiClockTimeSixOutline,
+    mdiClockTimeSevenOutline,
+    mdiClockTimeEightOutline,
+    mdiClockTimeNineOutline,
+    mdiClockTimeTenOutline,
+    mdiClockTimeElevenOutline,
+    mdiClockTimeTwelveOutline,
+  ].reverse()
 </script>
 
 <script lang="ts">
@@ -82,24 +112,34 @@
   export let forColor: Color
   const clock = getContext<Readable<ClockState>>("clock")
   $: seconds = $clock.remaining[forColor]
+  $: running = $clock.running && $clock.toMove === forColor
 </script>
 
-<div class="clock" class:running={$clock.running && $clock.toMove === forColor}>
+<div class="clock" class:running>
+  {#if running}
+    <Icon path={clockIcons[Math.floor(seconds % 12)]} />
+  {:else}
+    <Icon path={mdiPause} />
+  {/if}
+  <span>
   {#if seconds < 10}
     {seconds.toLocaleString(undefined, { minimumFractionDigits: 1, maximumFractionDigits: 1 })}
   {:else if seconds < 3600}
     {Math.floor(seconds / 60)}:{doubleDigit(seconds % 60)}
   {:else}
     {Math.floor(seconds / 3600)}:${doubleDigit(seconds % 3600 / 60)}:{doubleDigit(seconds % 60)}
-  {/if}
+  {/if}</span>
 </div>
 
 <style lang="scss">
   .clock {
     display: flex;
-    align-items: middle;
-    line-height: 1.6em;
-    padding: .25em .5em .1em;
+    align-items: center;
+    justify-content: center;
+    gap: .33em;
+    padding: .2em .33em;
+
+    font-weight: bold;
     background: #0008;
     border: 1px solid #fff4;
     border-radius: 3px;
