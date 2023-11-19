03fee0651e30c6730ae2fe70e3614f8dd08269df
philer <me@philer.org>
2023-11-19 17:25:23 +0100

    use vite-plugin-pwa to enable offline PWA

--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,41 @@
 import { svelte } from "@sveltejs/vite-plugin-svelte"
 import { defineConfig } from "vite"
+import { VitePWA } from "vite-plugin-pwa"
 
 // https://vitejs.dev/config/
-export default defineConfig(() => ({
+export default defineConfig(({ mode }) => ({
   base: "./",
   build: {
     target: "es2022",
   },
-  plugins: [svelte()],
+  plugins: [
+    svelte(),
+    VitePWA({
+      registerType: "autoUpdate",
+      injectRegister: "inline",
+      devOptions: { enabled: mode === "development" },
+      workbox: {
+        globPatterns: ["**/*.{js,css,html,ico,png,svg}"],
+      },
+      manifest: {
+        name: "phichess",
+        short_name: "phichess",
+        description: "Play over the board chess on any device",
+        display: "standalone",
+        start_url: "./",
+        scope: "./",
+        theme_color: "#222222",
+        background_color: "#f0d9b5",
+        icons: [
+          { src: "./phichess-512.png", sizes: "512x512", type: "image/png" },
+          { src: "./phichess-256.png", sizes: "256x256", type: "image/png" },
+          { src: "./phichess-192.png", sizes: "192x192", type: "image/png" },
+          { src: "./phichess-144.png", sizes: "144x144", type: "image/png" },
+          { src: "./phichess-128.png", sizes: "128x128", type: "image/png" },
+          { src: "./phichess-64.png", sizes: "64x64", type: "image/png" },
+          { src: "./phichess-32.png", sizes: "32x32", type: "image/png" },
+        ],
+      },
+    }),
+  ],
 }))
